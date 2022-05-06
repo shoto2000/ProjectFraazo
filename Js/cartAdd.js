edd83fb8a70431ec79c4e787cde5edf6fd913e35
shoto2000 <tayyabsyed2000@gmail.com>
2022-05-06 13:08:10 +0530

    Started

---
+++ Js/cartAdd.js
@@ -0,0 +1,19 @@
+var cart = JSON.parse(localStorage.getItem("cartadd")) || [];
+
+function Add(el) {
+    var count = 0;
+
+    cart.forEach(function (elem) {
+        if (el.name == elem.name) {
+            count++;
+        }
+    })
+    if (count == 0) {
+        cart.push(el);
+        localStorage.setItem("cartadd", JSON.stringify(cart));
+        alert("Product added Successfully");
+    }
+    else {
+        alert("product already in the cart");
+    }
+}
---
+++ Js/productcommon.js
@@ -0,0 +1,44 @@
+function display(data) {
+    data.forEach(function (el) {
+        var box = document.createElement("div");
+
+        var pimg = document.createElement("img");
+        pimg.src = el.img;
+
+        var pname = document.createElement("p");
+        pname.innerText = el.name;
+
+        var pquantity = document.createElement("p");
+        pquantity.innerText = el.quantity;
+
+        var pprice = document.createElement("h5");
+        pprice.innerText = "₹" + el.price;
+        pprice.style.fontSize = "16px";
+
+        var pstrike_price = document.createElement("h5");
+        if (el.strike_price == "") {
+            pstrike_price.innerText = el.strike_price;
+        }
+        else {
+            pstrike_price.innerText = "₹" + el.strike_price;
+        }
+
+        var cart = document.createElement("button");
+        cart.innerText = "ADD";
+        cart.addEventListener("click", function () {
+            Add(el);
+        })
+
+        var boxin = document.createElement("div");
+
+        var pricestore = document.createElement("div");
+
+        pricestore.append(pprice, pstrike_price, cart)
+
+        boxin.append(pname, pquantity, pricestore)
+
+        box.append(pimg, boxin);
+
+        document.querySelector(".product_card").append(box);
+    })
+}
